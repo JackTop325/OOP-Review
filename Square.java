3908acff71407792412514f966ca2a20d81ac766
JackTop325 <jackie.jiang22@gmail.com>
2021-12-16 17:15:02 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,4 +1,14 @@
+/**
+ * Square class that extends Rectangle class
+ * @author Jackie Jiang
+ * @version 1.0
+ * Date: Mar 10
+ */
 public class Square extends Rectangle{
+   /**
+    * Square constructor
+    * @param side
+    */
    public Square(int side){
       super("Square",side,side);
    }
